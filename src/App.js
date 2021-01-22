7e81bf7399278d378874228febaaa571e6f139fa
OjanayR <69225985+OjanayR@users.noreply.github.com>
2021-01-22 15:36:36 -0500

    changed pictures

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
   } 
 
   return (
-    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 20) ? 'app warm' : 'app') : 'app'}>
         <main>
           <div className="search-box">
             <input
@@ -61,7 +61,7 @@ function App() {
           </div>
           ) : ('')}
         </main>
-    </div>
+      </div>
   );
 }
 
